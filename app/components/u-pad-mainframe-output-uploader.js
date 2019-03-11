9419fc95f288cfc625310da58b58ef3ba1239c6d
allthesignals <wilburnforce@gmail.com>
2019-03-11 15:51:25 -0400

    init commit with app structure

---
+++ app/components/u-pad-mainframe-output-uploader.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import { action } from '@ember-decorators/object'; 
+
+export default class UPadMainframeOutputUploader extends Component {
+  @action
+  handleFiles() {
+    
+  }
+}
