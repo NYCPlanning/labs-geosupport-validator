e07de52acb90652f0d4ab5ad90d9f3ae6ea9ea20
allthesignals <wilburnforce@gmail.com>
2019-03-11 21:01:17 -0400

    Address fingerprint issue

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      enabled: false
+     prepend: 'https://allthesignals.github.io/labs-upad-validate/',
     },
   });
 
