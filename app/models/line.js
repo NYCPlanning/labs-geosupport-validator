9cc639c3fa8ff2a90e7af4328eb797ec7d479a8d
allthesignals <wilburnforce@gmail.com>
2019-03-24 20:42:08 -0400

    Enable polyfill; goat links; other lookups

---
+++ app/models/line.js
@@ -0,0 +1,121 @@
+import { setProperties } from '@ember/object';
+import { computed } from '@ember-decorators/object';
+import fetch from 'fetch';  
+
+const boroCodeLookup = {
+  '1': 'Manhattan',
+  '2': 'Bronx',
+  '3': 'Brooklyn',
+  '4': 'Queens',
+  '5': 'Staten Island',
+};
+const patterns = [{
+  fileNameMatcher: 'BL',
+  length: 56,
+  boroCodeLength: 13,
+}, {
+  fileNameMatcher: '1A',
+  length: 62,
+  boroCodeLength: 13,
+}, {
+  fileNameMatcher: 'BN',
+  length: 42,
+  boroCodeLength: 17,
+}];
+const GEOSEARCH_URL = 'https://geosearch.planninglabs.nyc/v1/autocomplete?text=';
+
+export default class Line {
+  constructor(props) {
+    setProperties(this, props);
+  }
+
+  currentHeader = '';
+  date = '';
+  line = '';
+  name = '';
+
+  @computed('line')
+  get asyncSearchResults() {
+    const extracted = this.line.slice(14, 61).trim();
+
+    if (this.pattern.fileNameMatcher === '1A') {
+      return fetch(`${GEOSEARCH_URL}${extracted}`)
+        .then(blob => blob.json());
+    }
+  }
+
+  @computed('name')
+  get pattern() {
+    // sniff out the file type for correct slicing pattern
+    return patterns
+      .find(({ fileNameMatcher }) => this.name.includes(fileNameMatcher));
+  }
+
+  @computed('pattern')
+  get fileNameMatcher() {
+    return this.pattern.fileNameMatcher;
+  }
+
+  @computed('line', 'currentHeader')
+  get id() {
+    return `${this.line} ${this.currentHeader}`;
+  }
+
+  @computed('boroCode')
+  get boroName() {
+    return boroCodeLookup[this.boroCode];
+  }
+
+  @computed('line')
+  get left() {
+    return this.line.slice(1,3);
+  }
+
+  @computed('line')
+  get right() {
+    return this.line.slice(this.pattern.length, this.pattern.length + 2);
+  }
+
+  @computed('line')
+  get boroCode() {
+    return this.line.slice(
+      this.pattern.boroCodeLength,
+      this.pattern.boroCodeLength + 1,
+    );
+  }
+
+  // this is sensitive to the type
+  // this makes it locatable, however,
+  // only in specific ways (address, BBL, BIN)
+  @computed('line', 'fileNameMatcher')
+  get location() {
+    // handling for line type
+    if (this.fileNameMatcher === '1A') {
+      const addressNumber = this.line.slice(14, 28).trim();
+      const street = this.line.slice(27, 61).trim();
+
+      return {
+        addressNumber,
+        street,
+      };
+    }
+
+    if (this.fileNameMatcher === 'BN') {
+      // this a manual note, may not work
+      const fuzzyAddress = this.line.split('/').reverse()[0];
+
+      return fetch(`${GEOSEARCH_URL}${fuzzyAddress}`)
+        .then(blob => blob.json())
+        .then(results => {
+          const { features: [firstResult] } = results;
+
+          return {
+            addressNumber: firstResult.properties.housenumber,
+            street: firstResult.properties.street,
+          };
+        });
+    }
+
+    return this.line;
+  }
+}
--- app/services/upad-files.js
+++ app/services/upad-files.js
@@ -1,42 +1,22 @@
 import Service from '@ember/service';
+import Line from 'labs-upad-validate/models/line';
 
 const FOLD = 'ERROR RECORDS: ';
-const boroCodeLookup = {
-  '1': 'Manhattan',
-  '2': 'Bronx',
-  '3': 'Brooklyn',
-  '4': 'Queens',
-  '5': 'Staten Island',
-};
-const patterns = [{
-  fileNameMatcher: 'BL',
-  length: 56,
-  boroCodeLength: 13,
-}, {
-  fileNameMatcher: '1A',
-  length: 62,
-  boroCodeLength: 13,
-}, {
-  fileNameMatcher: 'BN',
-  length: 42,
-  boroCodeLength: 17,
-}];
 const flatten = (array) => array.reduce((acc, curr) => acc.concat(curr), []);
 
 export default class UpadFilesService extends Service {
   files = [];
+
+  // array of Line classes
   lines = [];
 
   add(file) {
     const reader = new FileReader();
+
     reader.onload = (event) => {
       const { name: rawName } = file;
       const extractedName = (rawName.match(/[^(]+(?=\))/) || [])[0];
       const name = `${extractedName}.txt`;
-
-      // sniff out the file type for correct slicing pattern
-      const { length, boroCodeLength, fileNameMatcher } = patterns
-        .find(({ fileNameMatcher }) => name.includes(fileNameMatcher));
       const fileText = event.target.result;
 
       // push up the file
@@ -58,6 +38,7 @@ export default class UpadFilesService extends Service {
       );
 
       let currentHeader = '';
+
       // slice up the lines based on static patterns
       lines.forEach((line = '') => {
         const isHeader = line.includes(' GBAT ');
@@ -66,21 +47,12 @@ export default class UpadFilesService extends Service {
           currentHeader = line;
         }
 
-        const left = line.slice(1,3);
-        const right = line.slice(length, length + 2);
-        const boroCode = line.slice(boroCodeLength, boroCodeLength + 1);
-
-        this.lines.pushObject({
+        this.lines.pushObject(new Line({
           currentHeader,
           date,
-          left,
-          right,
           line,
           name,
-          fileType: fileNameMatcher,
-          boroName: boroCodeLookup[boroCode],
-          id: `${line} ${currentHeader}`,
-        });
+        }));
       });
     };
 
